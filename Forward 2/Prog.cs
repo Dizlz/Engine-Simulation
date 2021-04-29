a567816d03d6f13cd802808613851a044b4f3319
никита поливанский <nikita.polivansky@yandex.ru>
2021-04-29 17:23:25 +0700

    Добавьте файлы проекта.

---
+++ Forward 2/Prog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forward_2
+{
+    class Prog
+    {
+        static void Main()
+        {
+
+            //Вывод подробной информации
+            Console.Write("Хотите видеть подробную информацию?(Напишите Yes/No. Может повлиять на скорость работы программы!) ");
+            string More_Inf = Console.ReadLine();
+
+            //Температура окружающей среды
+            Console.Write("Температура окружающей среды: ");
+            double T_Surrounding = Convert.ToDouble(Console.ReadLine());
+
+            //Создание тестового стенда
+            Test_Area test_area = new Test_Area();
+
+            //Выбор двигателя
+            Console.WriteLine("Какой двигатель тестировать?(Введите номер)\n1)Стандартный \n2)Форсированный");
+            int engine_number =Convert.ToInt32( Console.ReadLine()); 
+
+            //Тестирование
+            Console.WriteLine(test_area.standart_test(T_Surrounding, More_Inf, engine_number));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Forward 2/Test_Area.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forward_2
+{
+    public class Test_Area
+    {
+        //Счетчик времени
+        int Time;
+
+        //Скорость коленвала
+        float V_Speed;
+       
+        //Температура двигателя
+        double T_Engine;
+                             
+        //Стандартный тест
+        public string standart_test(double T_Surrounding, string More_Inf, int engine_number)
+        {
+            T_Engine = T_Surrounding;
+            V_Speed = 0;            
+            Time = 0;
+
+            //Текущая зависимость M & V
+            int j = 0;
+            
+            //Инициализация двигателя
+            Engine engine= new Engine();
+                        
+            if (engine_number == 1 )
+            {
+                engine.standartengine();
+            }
+            else if (engine_number == 2)
+            {
+                engine.forcedengine();
+            }
+            else
+            {
+                return "Двигатель не найден";
+            }
+            
+            //Запуск и тестирование
+            while ((T_Engine < engine.T_Overheat) & (Time < 1000))
+            {
+               
+                engine.start_engine(T_Surrounding, ref V_Speed, ref T_Engine, ref j );
+
+                //Подсчет времени
+                Time++;
+
+                //Вывод дополнительной информации
+                if (More_Inf == "Yes")
+                {
+                    Console.WriteLine("Время: " + Time);
+                    Console.WriteLine("Температура двигателя: " + T_Engine);
+                    Console.WriteLine("Скорость коленвала:" + V_Speed);       
+                    Console.WriteLine("-------------------");
+                }
+            }
+            //Результат тестирования
+            if (Time >= 1000)
+            {
+                return "Двигатель работал в течении " + Time + " секунд. Перегрев не произошел";
+            }
+            return "Перегрев произошел спустя " + Time + " секунд после старта теста.";
+
+        }
+    }
+}
